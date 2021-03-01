14295f07bd6188eaf829b2a71366980e5f1f339b
AsrbekMahmudov <maxmudovasrbek777@gmail.com>
2021-03-01 21:20:05 +0500

    Initial Commit

---
+++ GithubDemo2.1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubDemo2.1
+//
+//  Created by Mahmudov Asrbek Ulug'bek o'g'li on 3/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
